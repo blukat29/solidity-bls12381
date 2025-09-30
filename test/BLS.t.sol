76f51bb703b4e0b9f42f6a8e8274a1095effe91b
ollie.j <ollie.j@klaytn.foundation>
2025-09-30 23:47:48 +0900

    test_Pairing

--- test/BLS.t.sol
+++ test/BLS.t.sol
@@ -16,14 +16,14 @@ contract BLSTest is Test {
 
     // Intermediate data calculated using '@noble/curves/bls12-381.js'
     bytes testSigBytesUncompressed = hex"0e82747ddeefe4fd64cf9cedb9b04ae3e8a43420cd255e3c7cd06a8d88b7c7f8638543719981c5d16fa3527c468c25f0026704a6951bde891360c7e8d12ddee0559004ccdbe6046b55bae1b257ee97f7cdb955773d7cf29adf3ccbb9975e4eb915e60d5b66a43e074b801a07df931a17505048f7f96dc80f857b638e505868dc008cc9c26ed5b8495e9c181b67dc4c2317d9d447337a9cc6d2956b9c6dd7c23c0bfb73855e902061bcb9cb9d40e43c38140091e638ffcffc7261366018900047";
-    uint256[4] testPubPoint = [
+    uint256[4] testPubPoint = [ // bls12_381.G1.Point.fromBytes(Buffer.from(pubHex,'hex'))
         0x153d21a4cfd562c469cc81514d4ce5a6,
         0xb577d8403d32a394dc265dd190b47fa9f829fdd7963afdf972e5e77854051f6f,
         0x14e22fd412a826a329fb40cbdc01b5e4,
         0xe2f931ed84d8e45932ec62a039f9d61a9dbf2c6eedc5db6fa585b6e0bdde100c
     ];
     // NOTE: The uncompressed bytes is in (x1 || x0 || y1 || y0) order. But EIP-2537 precompiles expect an array of (x0 || x1 || y0 || y1) order. Below arrays are in EIP-2537 order.
-    uint256[8] testSigPoint = [
+    uint256[8] testSigPoint = [ // bls12_381.G2.Point.fromBytes(Buffer.from(sigHex,'hex'))
         0x026704a6951bde891360c7e8d12ddee0,
         0x559004ccdbe6046b55bae1b257ee97f7cdb955773d7cf29adf3ccbb9975e4eb9,
         0x0e82747ddeefe4fd64cf9cedb9b04ae3,
@@ -33,7 +33,7 @@ contract BLSTest is Test {
         0x15e60d5b66a43e074b801a07df931a17,
         0x505048f7f96dc80f857b638e505868dc008cc9c26ed5b8495e9c181b67dc4c23
     ];
-    uint256[8] testMsgPoint = [
+    uint256[8] testMsgPoint = [ // bls12_381.longSignatures.hash(Buffer.from(msgHex,'hex'), dst)
         0x0bc0f071f2d0655a5edbf6b9208a6649,
         0xd3309b8692d2f55bde74c52cc2de0fed2bb60b4c45935b11c32827da1b80cb8f,
         0x179451d90ade914f7a6ffc5062914af9,
@@ -42,6 +42,12 @@ contract BLSTest is Test {
         0x1aedae63e5ecca4bba4013638718e0f02a7656ffcb1f93c419483f820dc07129,
         0x0c2309a40a73adc91fb43fcb8589048e,
         0x0141616d978e120dc4d354b502d93fe74443dec6a00d1f80a487ed4bdfea41c2
+    ];
+    uint256[4] NEG_G1 = [ // bls12_381.G1.Point.BASE.negate()
+        0x17f1d3a73197d7942695638c4fa9ac0f,
+        0xc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb,
+        0x114d1d6855d545a8aa7d76c8cf2e21f2,
+        0x67816aef1db507c96655b9d5caac42364e6f38ba0ecb751bad54dcd6b939c2ca
     ];
 
     function test_UnmarshalPub() public view {
@@ -84,5 +90,28 @@ contract BLSTest is Test {
             H[6] == testMsgPoint[6] &&
             H[7] == testMsgPoint[7]
         );
+    }
+
+    function test_Pairing() public view {
+        // e(P, H) == e(-G1, S)
+        uint256[4] memory P = testPubPoint;
+        uint256[8] memory H = testMsgPoint;
+        uint256[4] memory G = NEG_G1;
+        uint256[8] memory S = testSigPoint;
+
+        uint256[24] memory input = [
+            P[0], P[1], P[2], P[3],
+            H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7],
+            G[0], G[1], G[2], G[3],
+            S[0], S[1], S[2], S[3], S[4], S[5], S[6], S[7]
+        ];
+        console.log(input[0]);
+        uint256[1] memory out;
+        bool success;
+        assembly {
+            success := staticcall(gas(), 0x0f, input, 768, out, 32)
+        }
+        assert(success);
+        assert(out[0] != 0);
     }
 }
